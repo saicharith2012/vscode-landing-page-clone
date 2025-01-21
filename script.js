eee6a7f1cfe2034b675e75e58ae1e788bde5ef01
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-01-21 16:39:23 +0530

    feat: adding border transition to header based on scroll position

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // dismiss announcement functionality
-const dismissButton = document.querySelector(".dismiss")
-dismissButton.setAttribute("onclick", "removeAnnouncement()")
+const dismissButton = document.querySelector(".dismiss");
+dismissButton.setAttribute("onclick", "removeAnnouncement()");
 
 function removeAnnouncement() {
-    const announcementEl = document.querySelector(".announcement")
-    // console.log(announcementEl.parentNode)
-    const announcementContainer = announcementEl.parentNode
-    announcementContainer.removeChild(announcementEl)
-    announcementContainer.removeChild(dismissButton)
+  const announcementEl = document.querySelector(".announcement");
+  // console.log(announcementEl.parentNode)
+  const announcementContainer = announcementEl.parentNode;
+  announcementContainer.removeChild(announcementEl);
+  announcementContainer.removeChild(dismissButton);
 }
+
+// adding border to header based on scroll position
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  const triggerPoint = 0;
+
+  if (window.scrollY > triggerPoint) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
